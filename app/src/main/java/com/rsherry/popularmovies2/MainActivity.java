aad41b43cc597f4b6fbeb1cf23fdb20d6a91d346
Ryan Sherry <ryansherry@Ryans-MBP.domain>
2018-06-28 16:59:48 -0700

    made suggested and required changes

--- app/src/main/java/com/rsherry/popularmovies2/MainActivity.java
+++ app/src/main/java/com/rsherry/popularmovies2/MainActivity.java
@@ -26,21 +26,30 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements ListItemClickListener {
-    private static final String SAVED_LAYOUT_MANAGER = "SAVED_LAYOUT_MANAGER";
-    private static final String SAVED_MOVIE_LIST = "SAVED_MOVIE_LIST";
-    private static final String SAVED_SORTING_OPTION = "SAVED_SORTING_OPTION";
+    public static final String SAVED_LAYOUT_MANAGER = "SAVED_LAYOUT_MANAGER";
+    public static final String SAVED_MOVIE_LIST = "SAVED_MOVIE_LIST";
+    public static final String SAVED_SORTING_OPTION = "SAVED_SORTING_OPTION";
     public static final String SAVED_FAVORITE_LIST = "SAVED_FAVORITE_LIST";
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private static final String SAVED_HIGHEST_RATED = "SAVED_HIGHEST_RATED";
-    private static final String SAVED_MOST_POPULAR = "SAVED_MOST_POPULAR";
+    public static final String TAG = MainActivity.class.getSimpleName();
+    public static final String SAVED_HIGHEST_RATED = "SAVED_HIGHEST_RATED";
+    public static final String SAVED_MOST_POPULAR = "SAVED_MOST_POPULAR";
+
+    //Sorting by list options
+
+    public static final String MOST_POPULAR = "MOST_POPULAR";
+    public static final String HIGHEST_RATED = "HIGHEST_RATED";
+    public static final String FAVORITES = "FAVORITES";
+
     private MovieAdapter mAdapter;
     private static final String API_KEY = ApiKey.getApiKey();
-    RecyclerView mRecyclerView;
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
     RecyclerView.LayoutManager mLayoutManager;
     List<RetroMovie> mMovies;
     List<RetroMovie> mHighestRated;
@@ -48,18 +57,14 @@ public class MainActivity extends AppCompatActivity implements ListItemClickList
     List<RetroMovie> mFavorites;
     AppDatabase mDb;
     Parcelable mListState;
-
-    public enum SortingBy {
-        MOST_POPULAR, HIGHEST_RATED, FAVORITES
-    }
-
-    SortingBy mSortingBy;
+    String mSortingBy;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         mDb = AppDatabase.getsInstance(getApplicationContext());
 
@@ -68,11 +73,17 @@ public class MainActivity extends AppCompatActivity implements ListItemClickList
             mMovies = savedInstanceState.getParcelableArrayList(SAVED_MOVIE_LIST);
             mMostPopular = savedInstanceState.getParcelableArrayList(SAVED_MOST_POPULAR);
             mHighestRated = savedInstanceState.getParcelableArrayList(SAVED_HIGHEST_RATED);
-            String mSortingByValue = savedInstanceState.getString(SAVED_SORTING_OPTION);
-            mSortingBy = SortingBy.valueOf(mSortingByValue);
+            mSortingBy = savedInstanceState.getString(SAVED_SORTING_OPTION);
 
         } else {
-            mSortingBy = SortingBy.MOST_POPULAR;
+
+            //initialize incase null values
+            mMovies = new ArrayList<>();
+            mAdapter = new MovieAdapter(mMovies, this);
+            mAdapter.setMovies(mMovies);
+            mRecyclerView.setAdapter(mAdapter);
+
+            mSortingBy = MOST_POPULAR;
         }
 
         loadSavedFavorites();
@@ -83,8 +94,10 @@ public class MainActivity extends AppCompatActivity implements ListItemClickList
                 break;
             case HIGHEST_RATED:
                 sortByHighestRated();
+                break;
             case FAVORITES:
                 viewFavorites(mFavorites);
+                break;
         }
 
     }
@@ -96,13 +109,13 @@ public class MainActivity extends AppCompatActivity implements ListItemClickList
         mListState = mRecyclerView.getLayoutManager().onSaveInstanceState();
         outState.putParcelable(SAVED_LAYOUT_MANAGER, mListState);
 
-        //Saving Favorites list only so it can be passed to the detailActivity via an intent and used to determine if the movie is a favorite or not. The actual persistence of favorites is done via Room and ViewModel
+        // Saving Favorites list only so it can be passed to the detailActivity via an intent and used to determine if the movie is a favorite or not. The actual persistence of favorites is done via Room and ViewModel
 
         outState.putParcelableArrayList(SAVED_MOVIE_LIST, (ArrayList<RetroMovie>) mMovies);
 
         outState.putParcelableArrayList(SAVED_HIGHEST_RATED, (ArrayList<RetroMovie>) mHighestRated);
         outState.putParcelableArrayList(SAVED_MOST_POPULAR, (ArrayList<RetroMovie>) mMostPopular);
-        outState.putString(SAVED_SORTING_OPTION, mSortingBy.name());
+        outState.putString(SAVED_SORTING_OPTION, mSortingBy);
     }
 
     @Override
@@ -129,7 +142,7 @@ public class MainActivity extends AppCompatActivity implements ListItemClickList
     }
 
     private void viewFavorites(List<RetroMovie> favorites) {
-        mSortingBy = SortingBy.FAVORITES;
+        mSortingBy = FAVORITES;
         if (favorites != null) {
             mMovies = favorites;
         }
@@ -148,7 +161,7 @@ public class MainActivity extends AppCompatActivity implements ListItemClickList
                 Log.d(TAG, "Updating list of favorites from LiveData in ViewModel");
                 mFavorites = favoriteList;
 
-                if (mSortingBy == SortingBy.FAVORITES) {
+                if (mSortingBy.equals(FAVORITES)) {
                     mMovies = favoriteList;
                     mFavorites = favoriteList;
                     viewFavorites(mMovies);
@@ -162,7 +175,7 @@ public class MainActivity extends AppCompatActivity implements ListItemClickList
     }
 
     public void sortByHighestRated() {
-        mSortingBy = SortingBy.HIGHEST_RATED;
+        mSortingBy = HIGHEST_RATED;
         GetEndpointData service = RetrofitClentInstance.getRetrofitInstance().create(GetEndpointData.class);
 
         //Only make the network call if movies are not cached
@@ -190,13 +203,14 @@ public class MainActivity extends AppCompatActivity implements ListItemClickList
                     } else {
                         Toast.makeText(getApplicationContext(), "This is a software bug. Please contact the developer of this app to                        investigate", Toast.LENGTH_LONG).show();
                     }
+                    generateMovieList(mMovies);
                 }
             });
         }
     }
 
     public void sortByMostPopular() {
-        mSortingBy = SortingBy.MOST_POPULAR;
+        mSortingBy = MOST_POPULAR;
         GetEndpointData service = RetrofitClentInstance.getRetrofitInstance().create(GetEndpointData.class);
 
         //Test to see if there is a network or configuration issue with retrofit
@@ -226,13 +240,13 @@ public class MainActivity extends AppCompatActivity implements ListItemClickList
                     } else {
                         Toast.makeText(getApplicationContext(), "This is a software bug. Please contact the developer of this app to                        investigate", Toast.LENGTH_LONG).show();
                     }
+                    generateMovieList(mMovies);
                 }
             });
         }
     }
 
     private void generateMovieList(List<RetroMovie> movieList) {
-        mRecyclerView = findViewById(R.id.recyclerView);
         mAdapter = new MovieAdapter(movieList, this);
         mAdapter.setMovies(movieList);
         mRecyclerView.setAdapter(mAdapter);
